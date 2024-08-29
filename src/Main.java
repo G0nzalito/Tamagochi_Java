f72acc11a44dfe9eb474a9a8d21876763e1a48ba
G0nzalito <zavalagonzalo172@gmail.com>
2024-08-28 23:03:26 -0300

    Subimos casi tamagochi

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Mascota brenda = new Mascota(10, 5);   
+        brenda.presentacion();
+    }
+}
---
+++ src/Mascota.java
@@ -0,0 +1,124 @@
+public class Mascota {
+    private int energia;
+    private int humor;
+    private boolean dormido;
+    private int saciedad;
+
+    public Mascota(int energia, int humor) {
+        if (validarEnergia(energia)) {
+            this.energia = energia;
+        }else {
+            this.energia = 0;
+        }
+        if (validarHumor(humor)) {
+            this.humor = humor;
+        }else{
+            this.humor = 0;
+        }
+        this.dormido = false;
+    };
+
+    private boolean validarEnergia(int energia){
+        return energia > 0 && energia <= 100;
+    };
+    private boolean validarHumor(int humor){
+        return humor > 0 && humor <= 5;
+    };
+
+    //Metodos de ingesta
+    public void comer(){
+        if (!this.dormido) {
+            int energiaAumentada = (int) (energia * 1.10);
+            energia = Math.min(energiaAumentada, 100);
+            saciedad++;
+            if(saciedad >= 3 && humor != 0){
+                this.humor--;
+            }else {
+                if (humor != 5) {
+                    humor += 1;
+                }
+            }
+            ;
+        }
+    };
+
+    public void beber() {
+        if (!this.dormido) {
+            int energiaAumentada = (int) ((double) energia * 1.05);
+            energia = Math.min(energiaAumentada, 100);
+            saciedad++;
+            if(saciedad >= 3 && humor != 0) {
+                this.humor--;
+            }
+            if (humor != 5) {
+                humor += 1;
+        };
+        }
+    };
+
+    //Metodos de actividades
+
+    public void correr(){
+        if (!this.dormido) {
+            energia = (int) ((double) energia * 0.65);
+            if (humor != 0) {
+                humor -= 2;
+            }
+            if (saciedad > 0){
+                saciedad = 0;
+            }
+        }
+    };
+
+    public void saltar(){
+        if (!this.dormido) {
+            energia = (int) ((double) energia * 0.85);
+            if (humor != 0) {
+                humor -= 2;
+        }
+            if (saciedad > 0){
+                saciedad = 0;
+            }
+       }
+    };
+
+    //Metodos extra
+
+    public void dormir(){
+        this.dormido = true;
+        energia = Math.min((energia + 25), 100);
+        if(humor != 5){
+          humor += 2;
+        }
+        if (saciedad > 0){
+            saciedad = 0;
+        }
+    };
+
+    public void despertar(){
+        this.dormido = false;
+        this.humor -= 1;
+    };
+
+    public void presentacion(){
+        String humor = asignarHumor();
+        System.out.println("Hola, soy tu mascota, un gusto conocerte, tengo " + this.energia + " de energia y hoy estoy "
+                + humor  );
+    }
+
+    private String asignarHumor(){
+        String[] humores = new String[5];
+        humores[0] = "Muy enojado";
+        humores[1] = "Enojado";
+        humores[2] = "Neutral";
+        humores[3] = "Contento";
+        humores[4] = "Chocho";
+
+        if (humor != 0){
+            return humores[this.humor - 1];
+        }else {
+            return humores[0];
+        }
+    }
+
+}

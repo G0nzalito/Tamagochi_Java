0296bed05246e3fe3e9c1e27eab4ed8c446df2b4
Gonzalo <zavalagonzalo172@gmail.com>
2024-09-01 17:25:38 -0300

    Subimos cosas que hicimos el jueves en clase

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Mascota brenda = new Mascota(10, 5);   
-        brenda.presentacion();
+        Mascota brenda = new Mascota(10, 5);
+        Mascota gonza = new Mascota(5, 10);
+        gonza.presentacion();
+        gonza.fatality();
+        gonza.presentacion();
     }
 }
+//Brenda no corre nada sabes!
+//Brenda esta viva!!!!
